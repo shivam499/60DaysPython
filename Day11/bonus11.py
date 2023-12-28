a6ccd50d4e347d1258191fbdada677dcb9b31855
shivam499 <shivam.gpt499@gmail.com>
2023-12-28 22:43:40 +0530

    Day 11 completed.

---
+++ Day11/bonus11.py
@@ -0,0 +1,13 @@
+def get_average():
+    with open('temps.txt','r') as file:
+        data = file.readlines()[1:]
+
+        values = [float(i) for i in data]
+
+        average = sum(values)/ len(values)
+
+    return average
+
+
+avg = get_average()
+print(avg)
