6b1dc226280a1be30676a55ed871f49ff96d975f
Shivam Gupta <shivam.gpt499@gmail.com>
2024-01-07 00:14:50 +0530

    Day 15 Completed.

--- Todo_CLI/Day15/Excerise.py
+++ Todo_CLI/Day15/Excerise.py
@@ -3,6 +3,6 @@ import random
 lower = int(input("Enter the lower bound: "))
 upper = int(input("Enter the upper bound: "))
 
-number = random.randrange(lower,upper)
+number = random.randrange(lower, upper)
 
 print(number)
