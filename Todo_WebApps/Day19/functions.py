1b06433e54fe7726e892149ee909483277f30d52
Shivam Gupta <shivam.gpt499@gmail.com>
2024-01-07 20:23:31 +0530

    Day 19 Completed.

--- Todo_WebApps/Day19/functions.py
+++ Todo_WebApps/Day19/functions.py
@@ -1,6 +1,3 @@
-import streamlit as st
-
-
 def read_file(filename='todos.txt'):
     with open(filename, 'r') as file:  # with context manager
         return file.readlines()
@@ -17,8 +14,6 @@ def add_todo(todo):
     if len(new_todo.strip("\n")) > 0:
         todos.append(new_todo)
         write_file(todos)
-    # else:
-    #     st.warning("Todos is empty")
 
 
 def edit_todo(todo, old_todo):
--- Todo_WebApps/Day19/main.py
+++ Todo_WebApps/Day19/main.py
@@ -9,6 +9,14 @@ import functions as fn
 
 todos = fn.read_file()
 
+
+def add_todo():
+    new_todo = st.session_state["todo"] + "\n"
+    if len(new_todo.strip("\n")) > 0:
+        todos.append(new_todo)
+        fn.write_file(todos)
+
+
 st.title("To do App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
@@ -20,5 +28,5 @@ for todo in todos:
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo...", key="new_todo",
-              on_change=fn.add_todo(st.session_state["new_todo"]))
+st.text_input(label="", placeholder="Add new todo...", key="todo",
+              on_change=add_todo)
