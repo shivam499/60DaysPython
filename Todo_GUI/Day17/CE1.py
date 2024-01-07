94f986f146f62f9f309da63fb6a7c600c10381e8
Shivam Gupta <shivam.gpt499@gmail.com>
2024-01-07 12:46:49 +0530

    Day 17 Completed.

---
+++ Todo_GUI/Day17/CE1.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as gui
+
+
+def convert(feet, inch):
+    meter = feet * 0.3048 + inch * 0.0254
+    return meter
+
+
+label_feet = gui.Text("Enter feet:")
+label_inch = gui.Text("Enter inches:")
+
+feet_input = gui.Input("0", key="feet")
+inch_input = gui.Input("0", key="inch")
+
+btn_convert = gui.Button("Convert", key="convert")
+lbl_result = gui.Text(key="result")
+
+window = gui.Window("Convertor", layout=[[label_feet, feet_input], [label_inch, inch_input], [btn_convert, lbl_result]])
+
+while True:
+    event, values = window.read()
+
+    match event:
+        case "convert":
+            meters = f"{round(convert(float(values["feet"]), float(values["inch"])), 3)} m"
+            window["result"].update(value=meters)
+        case gui.WIN_CLOSED:
+            break
+
+window.close()
